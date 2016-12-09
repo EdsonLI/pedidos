af3cceb2725ea35025bf31a3290caf8c4a15530c
Edson Luis Isele <edsonisele@outlook.com>
2016-12-09 12:29:58 -0200

    Primeiro commit

---
+++ www/js/controllers.js
@@ -0,0 +1,70 @@
+angular.module('starter')
+  .controller('HomeController', function($scope, ProdutosService) {
+    ProdutosService.lista().then(function(dados) {
+      $scope.bolos = dados;
+    });
+    $scope.$on('produtos-atualizados', function(event, dados) {
+      $scope.bolos = dados;
+    });
+  });
+
+angular.module('starter').controller('DetalheController', function($scope, ProdutosService, $stateParams) {
+  ProdutosService.lista().then(function(dados) {
+    $scope.bolo = dados[$stateParams.boloId];
+  });
+});
+
+angular.module('starter').controller('PedidoController', function($scope, ProdutosService, $stateParams, $http,
+  $state, $ionicPopup, $ionicLoading) {
+  ProdutosService.lista().then(function(dados) {
+    $scope.bolo = dados[$stateParams.boloId];
+  });
+  $scope.dados = {};
+
+  $scope.fecharPedido = function() {
+    $ionicLoading.show();
+
+    $http.get('http://cozinhapp.sergiolopes.org/novo-pedido', {
+      params: {
+        pedido: $scope.bolo.nome,
+        info: $scope.dados.nome +
+          ' (' + $scope.dados.telefone + ') - ' +
+          $scope.dados.endereco
+      }
+    }).then(function() {
+      //caso ok, mostra pop-up confirmando e então navega pra home
+      $ionicPopup.alert({
+        title: "Pedido confirmado!",
+        template: "Daqui a pouco chega :)"
+      }).then(function() {
+        $state.go('home');
+      });
+    }).catch(function(erro) {
+      //caso de erro mostra alerta com erro
+      $ionicPopup.alert({
+        title: "Erro no pedido",
+        template: erro.data + ". Liga pra gente: 0000-9999"
+      });
+
+    }).finally(function() {
+      $ionicLoading.hide();
+    });
+  };
+
+});
+
+  /*.controller('HomeController', function($scope) {
+    $scope.bolos = [{
+      nome: "Só de Cenoura",
+      preco: 18
+    }, {
+      nome: "Com Nutella",
+      preco: 29
+    }, {
+      nome: "De Brigadeiro",
+      preco: 24
+    }, {
+      nome: "Açucarado",
+      preco: 19
+    }];
+  });*/
---
+++ www/js/services.js
@@ -0,0 +1,54 @@
+angular.module('starter')
+  .service('ProdutosService', function($http, $rootScope, $q) {
+    var url = 'http://cozinhapp.sergiolopes.org/produtos?random=1';
+    var onSuccess = function(position) {
+      var coords = position.coords;
+      console.log("Latitude: " + coords.latitude + ", Longitude: " + coords.longitude);
+      alert("Latitude: "+coords.latitude+", Longitude: "+coords.longitude);
+      var geocoder = new google.maps.Geocoder;
+      /*var latitudeLongitudeStr = '-28.235927,-52.357881';
+      var latlngStr = latitudeLongitudeStr.split(',', 2);*/
+      var latlng = {lat: parseFloat(coords.latitude), lng: parseFloat(coords.longitude)};
+      geocoder.geocode({'location': latlng}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          if (results[1]) {
+            alert(results[1].formatted_address);
+            //document.getElementById("endereco").value(results[1].formatted_address);
+            //infowindow.open(map, marker);
+          } else {
+            alert('No results found');
+          }
+        } else {
+          alert('Geocoder failed due to: ' + status);
+        }
+      });
+      //var json_endereco = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=-28.235927,-52.357881';
+      //console.info(json_endereco);
+    }
+    var onError = function(error) {
+      //alert("Code: "+error.code+"\nMensagem: "+error.message);
+    }
+
+    var promise = $http.get(url).then(function(response) {
+      var json = JSON.stringify(response.data);
+      localStorage.setItem('cache', json);
+      $rootScope.$broadcast('produtos-atualizados', response.data);
+      return response.data;
+    });
+
+    var cache = localStorage.getItem('cache');
+    if (cache != null) {
+      promise = $q(function(resolve, reject) {
+        resolve(JSON.parse(cache));
+      })
+    }
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, {timeout: 5000});
+
+    return {
+      lista: function() {
+        //return $http.get(url).then(function(response) {
+        return promise;
+        //});
+      }
+    }
+  });
